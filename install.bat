299d96d09236317fb88eba33680a0b1da70cc9a7
Ryan Bissett <ryanbissett@gamestop.com>
2019-10-07 18:17:53 +0100

    Added a check to see if Git is installed on install and uninstall

--- install.bat
+++ install.bat
@@ -1,12 +1,21 @@
 @echo off
 
-echo Copying commands to install directory
+git --version
 
-xcopy /s .\commands c:\git-gud\
+if not errorlevel 1 (
+    echo Copying commands to install directory
 
-echo Creating git aliases
+    xcopy /s .\commands c:\git-gud\
 
-for /f %%f in ('dir /b c:\git-gud') do (
-    git config --global alias.%%~nf !'C:\Users\rbissett\Documents\gitScripts\%%~nf.bat'
-    @echo Added git %%~nf
+    echo Creating git aliases
+
+    for /f %%f in ('dir /b c:\git-gud') do (
+        git config --global alias.%%~nf !'C:\Users\rbissett\Documents\gitScripts\%%~nf.bat'
+        @echo Added git %%~nf
+    )
+    pause
+) else (
+    echo Git doesn't appear to be available
+    echo Try adding git to your PATH variable or running this command from the location where you normaly use git
+    pause
 )
--- uninstall.bat
+++ uninstall.bat
@@ -1,14 +1,23 @@
 @echo off
 
-echo Removing git aliases
+git --version
 
-for /f %%f in ('dir /b .\commands') do (
-    git config --global --unset alias.%%~nf
-    @echo Removed git %%~nf
+if not errorlevel 1 (
+    echo Removing git aliases
+
+    for /f %%f in ('dir /b .\commands') do (
+        git config --global --unset alias.%%~nf
+        @echo Removed git %%~nf
+    )
+
+    echo Purging install directory
+
+    @RD /S /Q "c:\git-gud\"
+
+    echo Uninstall complete
+    pause
+) else (
+    echo Git doesn't appear to be available
+    echo Try adding git to your PATH variable or running this command from the location where you normaly use git
+    pause
 )
-
-echo Purging install directory
-
-@RD /S /Q "c:\git-gud\"
-
-echo Uninstall complete
