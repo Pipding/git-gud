eca0ca4903499e86ca415e619b5b223e12f4c5d2
Ryan Bissett <ryanbissett@gamestop.com>
2019-10-09 14:51:41 +0100

    Changing deleted text

--- windows/commands/purge.bat
+++ windows/commands/purge.bat
@@ -1,7 +1,7 @@
 @echo off
 
 IF [%1] == ["merged"] (
-    echo This command will delete all local branches that have been merged & echo This also includes local branches with no pushed changes
+    echo This command will delete all local branches that have been merged into master & echo This includes local branches with no pushed changes
 
     :choice
     set /P c=Are you sure you want to continue? [Y/N]
@@ -10,11 +10,11 @@ IF [%1] == ["merged"] (
 )
 
 IF [%1] == ["deleted"] (
-    echo This command will delete all local branches that have been merged & echo This also includes local branches with no pushed changes
+    echo This command will delete all local branches that have been deleted from the remote
 
     :choice
     set /P c=Are you sure you want to continue? [Y/N]
-    if /I "%c%" EQU "Y" goto :continueMerged
+    if /I "%c%" EQU "Y" goto :continueDeleted
     if /I "%c%" EQU "N" goto :cancel
 ) ELSE (
     echo git purge requires an argument & echo Usage: & echo git purge deleted & echo git purge merged
@@ -37,8 +37,8 @@ echo Switching to master
 git checkout master
 git pull
 echo Pruning remote
-git remote update origin --prune
-echo Removing local branches with merged upstreams
+git remote prune origin
+echo Removing local branches withoutupstreams
 for /f "usebackq" %%B in (`git branch --merged^|findstr /v /c:"* " /c:"master"`) do @git branch -d %%B
 echo Remaining branches:
 git branch -vv
